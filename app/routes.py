4a5437e8a3a6a61f6a938adcba7fff2b4f0bbb5e
m1roxx <nugmanovilyas228@gmail.com>
2025-02-11 13:16:28 +0500

    added home page

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('home.html')
 
 @main.route('/graphical-method')
 def graphical_method():
